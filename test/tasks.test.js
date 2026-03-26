3c07adcfef1c119a48fb0bbaeb089430238d9834
Pedro <pfernandez@agromas.pe>
2026-03-26 11:47:53 -0500

    feat: add getByDateRange to task service

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -104,3 +104,25 @@ test('fail update if task does not exist', () => {
     });
 });
 
+test('get tasks by date range', () => {
+    const storage = new MemoryStorage();
+    const service = new TaskService(storage);
+
+    service.create({
+        title: 'Task 1',
+        dueDate: '2026-03-26T10:00:00.000Z',
+    });
+
+    service.create({
+        title: 'Task 2',
+        dueDate: '2026-03-30T10:00:00.000Z',
+    });
+
+    const results = service.getByDateRange(
+        '2026-03-25T00:00:00.000Z',
+        '2026-03-27T23:59:59.000Z'
+    );
+
+    assert.equal(results.length, 1);
+    assert.equal(results[0].title, 'Task 1');
+});
